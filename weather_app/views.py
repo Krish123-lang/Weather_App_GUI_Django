d1398a56c00d900f7b0a6350aa8a2a67a2b26990
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2023-06-08 23:20:51 +0545

    Update views.py

--- weather_app/views.py
+++ weather_app/views.py
@@ -17,18 +17,26 @@ def home(request):
     r = requests.get(f"{base_url}{extension}?key={api_key}&q={place}")
 
     data = r.json()
-    time_obj = datetime.now(pytz.timezone(data['location']['tz_id']))
-    hours = int(time_obj.strftime('%I'))
+    time_obj = data['location']['localtime']
 
+    lat, lon = data["location"]["lat"], data["location"]["lon"]
+    condition = data["current"]["condition"]["text"]
+    wind_mph, wind_kph = data["current"]["wind_mph"], data["current"]["wind_kph"]
+    humidity = data["current"]["humidity"]
 
     messages.success(request, "Thank you for using this app !!! ")
 
     context = {
         "place": place,
         "locs": data['location']['tz_id'],
-        "times": datetime.now(),
-        "hours": hours,
+        "times": time_obj,
         "temps_c": data['current']['temp_c'],
         "temps_f": data['current']['temp_f'],
+        "lat": lat,
+        "lon": lon,
+        "condition": condition,
+        "wind_mph": wind_mph,
+        "wind_kph": wind_kph,
+        "humidity": humidity
     }
     return render(request, "home.html", context)
